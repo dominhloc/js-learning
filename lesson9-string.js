8d59442e7ce84b0af56dcab65f7460734685ac1b
dominhloc <dominhloc2510@gmail.com>
2024-05-04 00:16:27 +0700

    first commit

--- lesson9-string.js
+++ lesson9-string.js
@@ -1,1 +1,15 @@
 // Bài 9: Kiểm tra 1 chuỗi có phải là chuỗi viết hoa hay không?
+
+
+function myFunction(str) {
+    if (typeof str === "string") {
+        return str === str.toUpperCase();
+    }
+}
+console.log(myFunction("Hello"))
+console.log(myFunction("HELLO"))
+console.log(myFunction(1234))
+console.log(myFunction("1234"))
+console.log(myFunction(null))
+
+
